8b158abcc097231e2b0a17ae557d02d7f888fb1d
vedantmahajan123 <22070129@ycce.in>
2024-08-07 00:05:46 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-export {app}
+app.use(express.json({limit: "16kb"})) //can change limit
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public")) //to store files like  pdf,image etc
+app.use(cookieParser())
+
+
+
+
+export { app }
